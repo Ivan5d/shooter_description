46b386b9c43a5612ff2fe975b980ddb7e20558f9
Ivan5d <ivan.d557d@gmail.com>
2020-11-02 13:25:53 -0600

    talker.py and listener.py functions

--- shooter_gazebo/scripts/listener.py
+++ shooter_gazebo/scripts/listener.py
@@ -4,10 +4,10 @@ import rospy
 
 from std_msgs.msg import String
     
-    def callback(data):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
         
-    def listener():
+def listener():
     
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
@@ -15,12 +15,12 @@ from std_msgs.msg import String
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
 
-        rospy.init_node('listener', anonymous=True)
+    rospy.init_node('listener', anonymous=True)
    
-        rospy.Subscriber("topico_llamado_chatter", String, callback)
+    rospy.Subscriber("topico_llamado_chatter", String, callback)
    
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
    
-    if __name__ == '__main__':
-        listener()
+if __name__ == '__main__':
+    listener()
--- shooter_gazebo/scripts/talker.py
+++ shooter_gazebo/scripts/talker.py
@@ -5,24 +5,24 @@ import rospy
 #Import the type of message type
 from std_msgs.msg import String
    
-    def talker():
-        #Publica: topic named chatter, type String, 
-        pub = rospy.Publisher('topic_llamado_chatter', String, queue_size=10)
+def talker():
+    #Publica: topic named chatter, type String, 
+    pub = rospy.Publisher('topico_llamado_chatter', String, queue_size=10)
 
-        #Inicia el nodo
-        rospy.init_node('talker', anonymous=True)
+    #Inicia el nodo
+    rospy.init_node('talker', anonymous=True)
 
-        #10 veces por segundo
-        rate = rospy.Rate(10) 
-        
-        while not rospy.is_shutdown():
-           hello_str = "hello world %s" % rospy.get_time()
-           rospy.loginfo(hello_str)
-           pub.publish(hello_str)
-           rate.sleep()
+    #10 veces por segundo
+    rate = rospy.Rate(10) 
+
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
    
-    if __name__ == '__main__':
-        try:
-            talker()
-        except rospy.ROSInterruptException:
-            pass
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass

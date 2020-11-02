04c111fe6b998d16deba20ee255e02911c0e323d
Ivan5d <ivan.d557d@gmail.com>
2020-11-02 13:09:22 -0600

    Talker and publisher

--- shooter_gazebo/scripts/talker.py
+++ shooter_gazebo/scripts/talker.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
-# license removed for brevity
-    import rospy
-    from std_msgs.msg import String
+
+import rospy
+
+#Import the type of message type
+from std_msgs.msg import String
    
     def talker():
-        pub = rospy.Publisher('chatter', String, queue_size=10)
+        #Publica: topic named chatter, type String, 
+        pub = rospy.Publisher('topic_llamado_chatter', String, queue_size=10)
+
+        #Inicia el nodo
         rospy.init_node('talker', anonymous=True)
-        rate = rospy.Rate(10) # 10hz
+
+        #10 veces por segundo
+        rate = rospy.Rate(10) 
+        
         while not rospy.is_shutdown():
            hello_str = "hello world %s" % rospy.get_time()
            rospy.loginfo(hello_str)
